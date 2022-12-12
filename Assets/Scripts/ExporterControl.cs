6be39ea99e3289985b350de17172e1a4d8898fc5
NeilMcConnell <57053130+NeilMcConnell@users.noreply.github.com>
2022-12-11 23:58:20 -0500

    starting on export sequence
    doesn't work yet - anims don't seem to be advancing to the correct point

--- Assets/Scripts/ExporterControl.cs
+++ Assets/Scripts/ExporterControl.cs
@@ -11,9 +11,27 @@ struct ARGB32
     public byte A, R, G, B;
 }
 
+class WorkItem
+{
+    public AnimationClip clip;
+    public float AnimationTimeSeconds;
+    public Quaternion Rotation;
+    public string FileName;
+
+    public enum Step
+    {
+        Setup,
+        ExportColor,
+        ExportNormal,
+        Complete
+    }
+    public Step CurrentStep;
+}
+
 
 public class ExporterControl : MonoBehaviour
 {
+    public int FPS;
     public Camera Camera;
     private Camera ExportCamera;
     public GameObject ExportObject;
@@ -21,15 +39,20 @@ public class ExporterControl : MonoBehaviour
     public Shader NormalShader;
     private RenderTexture renderTexture;
 
+    public int ExportWidth = 1024;
+    public int ExportHeight = 1024;
 
 
-    bool ShouldExport = true;
+
+    //bool ShouldExport = true;
+    private string exportPath;
 
 
+    //AsyncGPUReadbackRequest req;
+    //bool reqDone = false;
+    //NativeArray<ARGB32> colorInfo;
 
-    AsyncGPUReadbackRequest req;
-    bool reqDone = false;
-    NativeArray<ARGB32> colorInfo;
+    private List<WorkItem> workItems;
 
     // Start is called before the first frame update
     void Start()
@@ -40,66 +63,155 @@ public class ExporterControl : MonoBehaviour
         ExportCamera = exportCameraGameObject.AddComponent<Camera>();        
         ExportCamera.CopyFrom(Camera);
 
-        renderTexture = new RenderTexture(1024, 1024, 32, RenderTextureFormat.ARGB32, 0);
-    }
+        renderTexture = new RenderTexture(ExportWidth, ExportHeight, 32, RenderTextureFormat.ARGB32, 0);
 
-    //int frames = 0;
-
-    // Update is called once per frame
-    void Update()
-    {
-        //++frames; //hack for testing
-        //float scaledFrames = frames / 1000f;
-        //Camera.transform.position = new Vector3(Mathf.Sin(scaledFrames) * 10, 5, Mathf.Cos(scaledFrames) * 10);
-        //Camera.transform.LookAt(ExportObject.transform);
-
-
-
-        if (ShouldExport)
+        var animator = ExportObject.GetComponent<Animator>();
+        
+        Debug.Log("clip info count " + animator.runtimeAnimatorController.animationClips.Length);
+        workItems = new List<WorkItem>();
+        foreach(AnimationClip clip in animator.runtimeAnimatorController.animationClips)
         {
-            ShouldExport = false;
-
-            RenderTexture.active = renderTexture;
-            ExportCamera.targetTexture = renderTexture;
-            //to review - SetReplacementShader, RenderRequest.  Probably not needed, if RenderWithShader can be used from the main thread, I guess?
-            ExportCamera.RenderWithShader(NormalShader, null);
-
-            Texture2D renderedTexture = new Texture2D(1024, 1024, TextureFormat.ARGB32, false);
-            renderedTexture.ReadPixels(new Rect(0, 0, 1024, 1024), 0, 0, false);
-            renderedTexture.Apply();
-
-            byte[] tga = renderedTexture.EncodeToTGA();
-            string path = Path.Combine(  Path.GetDirectoryName(Application.dataPath), "Exports");
-            string filename = Path.Combine(path, "proofOfConcept2.tga");
-            Directory.CreateDirectory(path);
-            Debug.Log(filename);
-            File.WriteAllBytes(filename, tga);
-
-            if (ColorShader == null)
-            {
-                ExportCamera.Render();
-            }
-            else
-            {
-                ExportCamera.RenderWithShader(ColorShader, null);
-            }
-            renderedTexture.ReadPixels(new Rect(0, 0, 1024, 1024), 0, 0, false);
-            renderedTexture.Apply();
-             tga = renderedTexture.EncodeToTGA();
-            filename = Path.Combine(path, "proofOfConcept1.tga");
-            File.WriteAllBytes(filename, tga);
-
-
-            //colorInfo = new NativeArray<ARGB32>(1024 * 1024, Allocator.Persistent);
-            //req = AsyncGPUReadback.RequestIntoNativeArray(ref colorInfo, renderedTexture);
+            Debug.Log(clip.name);
+            AddClipToExportList(clip);
         }
 
 
+        exportPath = Path.Combine(Path.GetDirectoryName(Application.dataPath), "Exports");
 
-        //if (!reqDone && req.done)
-        //{
-        //    reqDone = true;
-        //
-        //}
+    Directory.CreateDirectory(exportPath);
+
     }
+
+    private void AddClipToExportList(AnimationClip clip)
+    {
+        int[] rotations = {0, 45, 90, 135, 180, 225, 270, 315};
+        Quaternion originalRotation = ExportObject.transform.rotation;
+
+        int frameCount = Mathf.RoundToInt(clip.length * FPS);
+        float frameDuration = clip.length / frameCount;
+        if (!clip.isLooping)
+            frameCount++;//for non-looping, we want a frame right at the end of the anim
+        frameCount = Mathf.Max(frameCount, 1); //make sure we export at least one frame
+
+        Debug.Log($"for clip {clip.name}, exported {frameCount} frames at {1 / frameDuration} FPS (target was {FPS})");
+
+        foreach (int rotation in rotations)
+        {
+            for (int frame = 0; frame < frameCount; ++frame)
+            {
+                var work = new WorkItem();
+                work.AnimationTimeSeconds = frame * frameDuration;
+                work.Rotation = originalRotation * Quaternion.Euler(0, rotation, 0);
+                work.clip = clip;
+                work.FileName = $"{ExportObject.name}_{clip.name}_R{rotation:D3}_{frame:D3}";
+                Debug.Log(work.FileName);
+                workItems.Add(work);
+            }
+        }
+    }
+
+
+    // Update is called once per frame
+ //   void OriginalTestUpdate()
+ //   {
+ //       if (ShouldExport)
+ //       {
+ //           ShouldExport = false;
+ //
+ //           RenderTexture.active = renderTexture;
+ //           ExportCamera.targetTexture = renderTexture;
+ //           //to review - SetReplacementShader, RenderRequest.  Probably not needed, if RenderWithShader can be used from the main thread, I guess?
+ //           ExportCamera.RenderWithShader(NormalShader, null);
+ //
+ //           Texture2D renderedTexture = new Texture2D(ExportWidth, ExportHeight, TextureFormat.ARGB32, false);
+ //           renderedTexture.ReadPixels(new Rect(0, 0, ExportWidth, ExportHeight), 0, 0, false);
+ //           renderedTexture.Apply();
+ //
+ //           byte[] tga = renderedTexture.EncodeToTGA();
+ //           string path = Path.Combine(  Path.GetDirectoryName(Application.dataPath), "Exports");
+ //           string filename = Path.Combine(path, "proofOfConcept2.tga");
+ //           Directory.CreateDirectory(path);
+ //           Debug.Log(filename);
+ //           File.WriteAllBytes(filename, tga);
+ //
+ //           if (ColorShader == null)
+ //           {
+ //               ExportCamera.Render();
+ //           }
+ //           else
+ //           {
+ //               ExportCamera.RenderWithShader(ColorShader, null);
+ //           }
+ //           renderedTexture.ReadPixels(new Rect(0, 0, ExportWidth, ExportHeight), 0, 0, false);
+ //           renderedTexture.Apply();
+ //            tga = renderedTexture.EncodeToTGA();
+ //           filename = Path.Combine(path, "proofOfConcept1.tga");
+ //           File.WriteAllBytes(filename, tga);
+ //
+ //
+ //           //colorInfo = new NativeArray<ARGB32>(1024 * 1024, Allocator.Persistent);
+ //           //req = AsyncGPUReadback.RequestIntoNativeArray(ref colorInfo, renderedTexture);
+ //       }
+ //
+ //
+ //
+ //       //if (!reqDone && req.done)
+ //       //{
+ //       //    reqDone = true;
+ //       //
+ //       //}
+ //   }
+
+    private void Update()
+    {
+        if (workItems.Count == 0)
+            return;
+
+        var work = workItems[0];
+        switch(work.CurrentStep)
+        {
+            case WorkItem.Step.Setup:
+                {
+                    ExportObject.transform.rotation = work.Rotation;
+                    var animator = ExportObject.GetComponent<Animator>();
+                    animator.PlayInFixedTime(work.clip.name, -1, work.AnimationTimeSeconds);
+                    animator.speed = 0;
+                    work.CurrentStep++;
+                }
+                break;
+            case WorkItem.Step.ExportColor:
+                {
+
+                    RenderTexture.active = renderTexture;
+                    ExportCamera.targetTexture = renderTexture;
+
+                    if (ColorShader == null)
+                    {
+                        ExportCamera.Render();
+                    }
+                    else
+                    {
+                        ExportCamera.RenderWithShader(ColorShader, null);
+                    }
+
+                    Texture2D renderedTexture = new Texture2D(ExportWidth, ExportHeight, TextureFormat.ARGB32, false);
+                    renderedTexture.ReadPixels(new Rect(0, 0, ExportWidth, ExportHeight), 0, 0, false);
+                    renderedTexture.Apply();
+
+                    byte[] tga = renderedTexture.EncodeToTGA();
+                    string filename = Path.Combine(exportPath, work.FileName +  ".tga");
+                    File.WriteAllBytes(filename, tga);
+                    work.CurrentStep++;
+                }
+                break;
+            case WorkItem.Step.ExportNormal:
+                work.CurrentStep++;
+                break;
+            case WorkItem.Step.Complete:
+                workItems.RemoveAt(0);
+                break;
+        }
+
+    }
+
 }
